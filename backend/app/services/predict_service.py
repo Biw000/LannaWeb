0aabf9770e0ad67781635b0bd8173172d7f784d7
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-15 06:43:05 +0700

    Update predict_service.py

--- backend/app/services/predict_service.py
+++ backend/app/services/predict_service.py
@@ -1,16 +1,9 @@
 from app.ml.model_loader import load_model
 from app.ml.inference import predict
-import torch
 
 # โหลด model ครั้งเดียว
 model = load_model()
 
-async def predict_image(image):
-    try:
-        with torch.no_grad():
-            result = predict(model, image)
-        return result
-    except Exception as e:
-        return {
-            "error": str(e)
-        }
+def predict_image(image):
+    result = predict(model, image)
+    return result
