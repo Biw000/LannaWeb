05dd824eb99b0236eaa0b14facebecb19989cf6c
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-15 06:43:31 +0700

    Update inference.py

--- backend/app/ml/inference.py
+++ backend/app/ml/inference.py
@@ -1,24 +1,4 @@
-import torch
-import io
-from PIL import Image
-from torchvision import transforms
-import json
-import os
-
-# โหลด class names
-BASE_DIR = os.path.dirname(__file__)
-with open(os.path.join(BASE_DIR, "classes.json"), "r", encoding="utf-8") as f:
-    CLASS_NAMES = json.load(f)
-
-
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
-
-def predict(model, image_bytes):
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+def predict(model, image):
     image = transform(image).unsqueeze(0)
 
     with torch.no_grad():
@@ -35,5 +15,5 @@ def predict(model, image_bytes):
 
     return {
         "best_prediction": results[0],
-        "top_3": results
+        "top3": results
     }
