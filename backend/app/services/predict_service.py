5d3394e06c6ea76dd084efd3f576a51f45275e25
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-15 06:46:49 +0700

    Update predict_service.py

--- backend/app/services/predict_service.py
+++ backend/app/services/predict_service.py
@@ -1,9 +1,16 @@
 from app.ml.model_loader import load_model
 from app.ml.inference import predict
 
-# โหลด model ครั้งเดียว
-model = load_model()
+_model = None
+
+def get_model():
+    global _model
+    if _model is None:
+        print("Loading model...")
+        _model = load_model()
+    return _model
+
 
 def predict_image(image):
-    result = predict(model, image)
-    return result
+    model = get_model()
+    return predict(model, image)
