05ff4fe29413773f3eeaceea66f92bc5a7d309f7
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-14 20:09:54 +0700

    Update predict_service.py

--- backend/app/services/predict_service.py
+++ backend/app/services/predict_service.py
@@ -1,9 +1,8 @@
 from app.ml.model_loader import load_model
 from app.ml.inference import predict
 
-model = load_model()
-
 async def predict_image(file):
+    model = load_model()  
     image_bytes = await file.read()
     result = predict(model, image_bytes)
     return result
