c43c298f56127cdddc5a1a0b743455179aadb6aa
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-14 20:16:26 +0700

    Update predict_service.py

--- backend/app/services/predict_service.py
+++ backend/app/services/predict_service.py
@@ -2,11 +2,15 @@ from app.ml.model_loader import load_model
 from app.ml.inference import predict
 import torch
 
+
 async def predict_image(file):
+
+    # โหลดโมเดลตอนใช้งานเท่านั้น
     model = load_model()
 
     image_bytes = await file.read()
 
+    # ปิด gradient ลด RAM usage
     with torch.no_grad():
         result = predict(model, image_bytes)
 
