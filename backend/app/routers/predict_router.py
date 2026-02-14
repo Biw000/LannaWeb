23d387b13d4dc5a9af50e0c95e5ddc9f072c2be2
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-15 06:26:02 +0700

    Update predict_router.py

--- backend/app/routers/predict_router.py
+++ backend/app/routers/predict_router.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, UploadFile, File
+from PIL import Image
+import io
+
 from app.services.predict_service import predict_image
 
-router = APIRouter(
-    prefix="/predict",
-    tags=["Predict"]
-)
+router = APIRouter(prefix="/predict", tags=["Prediction"])
 
 @router.post("/")
 async def predict(file: UploadFile = File(...)):
-    return await predict_image(file)
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+    predicted_class, confidence = predict_image(image)
+
+    return {
+        "predicted_class": predicted_class,
+        "confidence": round(confidence, 4)
+    }
