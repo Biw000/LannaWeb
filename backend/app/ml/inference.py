1062e40bfa086c56434f9e80edf48602a6aca63c
Tanakorn Patanajaroenkorn <154036983+Biw000@users.noreply.github.com>
2026-02-15 13:06:41 +0700

    Update inference.py

--- backend/app/ml/inference.py
+++ backend/app/ml/inference.py
@@ -3,12 +3,11 @@ from torchvision import transforms
 import json
 import os
 
-# โหลด class names
 BASE_DIR = os.path.dirname(__file__)
+
 with open(os.path.join(BASE_DIR, "classes.json"), "r", encoding="utf-8") as f:
     CLASS_NAMES = json.load(f)
 
-# transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -20,16 +19,10 @@ def predict(model, image):
     with torch.no_grad():
         outputs = model(image)
         probabilities = torch.softmax(outputs, dim=1)
-        top_prob, top_class = torch.topk(probabilities, 3)
 
-    results = []
-    for i in range(3):
-        results.append({
-            "class_name": CLASS_NAMES[top_class[0][i].item()],
-            "confidence": round(top_prob[0][i].item(), 4)
-        })
+        confidence, predicted = torch.max(probabilities, 1)
 
     return {
-        "best_prediction": results[0],
-        "top3": results
+        "class_name": CLASS_NAMES[predicted.item()],
+        "confidence": round(confidence.item(), 4)
     }
